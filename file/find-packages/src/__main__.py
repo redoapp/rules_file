b8f542f8ede1664fcdb5fa048489c42fcb80afaa
Paul Draper <paulddraper@gmail.com>
2025-03-22 09:22:37 -0600

    Make find-packages match file name case-insensitively

--- file/find-packages/src/__main__.py
+++ file/find-packages/src/__main__.py
@@ -8,10 +8,14 @@ parser.add_argument("roots", nargs="*")
 parser.add_argument("--prefix", default="")
 args = parser.parse_args()
 
+# https://github.com/bazelbuild/bazel/blob/4c2d91e762ab6e492853b021408129dd93fb5904/src/main/java/com/google/devtools/build/lib/skyframe/BazelSkyframeExecutorConstants.java#L30
+# case-insensitive, have seen that matter
+build_names = {name.lower() for name in ("BUILD", "BUILD.bazel")}
+
 
 def packages(root):
     for root, _, files in walk(args.root / root):
-        if "BUILD" in files or "BUILD.bazel" in files:
+        if any(file.lower() in build_names for file in files):
             yield Path(root).relative_to(args.root)
 
 
@@ -20,7 +24,5 @@ for root in args.roots:
         print(
             args.prefix
             if str(package) == "."
-            else f"{args.prefix}/{package}"
-            if args.prefix
-            else package
+            else f"{args.prefix}/{package}" if args.prefix else package
         )
