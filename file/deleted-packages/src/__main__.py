4391f8dec5a9ddc4498c2a28ddefeddf0dde683b
Paul Draper <paulddraper@gmail.com>
2025-06-21 08:35:28 -0600

    Use multiple flags for deleted_packages
    Lessens chance of merge conflict. Requires newer Bazel version.

--- file/deleted-packages/src/__main__.py
+++ file/deleted-packages/src/__main__.py
@@ -5,9 +5,10 @@ parser = ArgumentParser(prog="deleted-packages")
 parser.add_argument("--config")
 args = parser.parse_args()
 
-packages = ",".join(line.rstrip() for line in stdin)
+packages = [line.rstrip() for line in stdin]
 
 for command in ["build", "query"]:
     if args.config:
         command = f"{args.config}:{command}"
-    print(command, f"--deleted_packages={packages}")
+    for package in packages:
+        print(command, f"--deleted_packages={package}")
